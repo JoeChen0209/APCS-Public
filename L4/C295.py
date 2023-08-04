f0702361012fc1a16cf805c48e7e34b40f2a5a2f
Joseph Chen(home) <s940390@gmail.com>
2023-08-04 23:40:29 +0800

    add zerojudge practice
    add zerojudge practice

---
+++ L4/C295.py
@@ -0,0 +1,35 @@
+# C295
+
+# 題目
+# 有N群
+# 每群都有M個整數
+# S=從每群中選出最大的數字加總起來
+# 在所有最大數裡面找出可以整除S的print出來
+
+# Input
+mn = input()
+data = []
+mn_list = list(map(int, mn.split()))
+m = mn_list[0]
+n = mn_list[1]
+for i in range(m):  # 輸入幾行(利用二維陣列的方式儲存)
+    data.append(list(map(int, input().split())))
+
+# Process
+# 找出各集合的最大數並加總起來
+max_data = []
+s = 0
+for i in range(len(data)):
+    max_data.append(max(data[i]))
+s = sum(max_data)
+print(s)
+
+# S去除每次找到的數字檢查是否可以整除
+num_str = ""
+for i in max_data:
+    if s % i == 0:
+        num_str = num_str+str(i)+" "
+if num_str == "":
+    print("-1")
+else:
+    print(num_str.strip())
---
+++ L4/C461.py
@@ -0,0 +1,30 @@
+# C461
+
+output = []
+data = list(map(int, input().split()))
+if data[0] != 0:
+    a = 1
+else:
+    a = data[0]
+if data[1] != 0:
+    b = 1
+else:
+    b = data[1]
+c = data[2]
+
+
+# 判斷and
+if (a and b) == c:
+    output.append("AND")
+# 判斷OR
+if (a or b) == c:
+    output.append("OR")
+# 判斷XOR(兩數相減為零)
+if (abs(a-b)) == c:
+    output.append("XOR")
+# 都沒有就是IMPOSSIBLE
+if len(output) == 0:
+    print("IMPOSSIBLE")
+else:
+    for o in output:
+        print(o)
